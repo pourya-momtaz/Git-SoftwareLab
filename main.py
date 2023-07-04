4e0c06c7a99ebd30c67109826a636b2c4dfe5123
pourya-momtaz <pouryamz19@gmail.com>
2023-07-04 21:04:57 +0330

    Added labels in statements of main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from circle import Circle
 from rectangle import Rectangle
 
 circle = Circle(5)
-print(circle.area())
+print("This is area:", circle.area())
 print(circle.perimeter())
 
 rectangle = Rectangle(4, 6)
 print(rectangle.area())
-print(rectangle.perimeter())
+print("This is perimeter:", rectangle.perimeter())
