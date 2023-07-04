74d7eb4dd8d22e742be24d49675a4b821b39a5bb
pourya-momtaz <pouryamz19@gmail.com>
2023-07-04 20:49:43 +0330

    Add print statements for perimeters in main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from rectangle import Rectangle
 
 circle = Circle(5)
 print(circle.area())
+print(circle.perimeter())
 
 rectangle = Rectangle(4, 6)
 print(rectangle.area())
+print(rectangle.perimeter())
