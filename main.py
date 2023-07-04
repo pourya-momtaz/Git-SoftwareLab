504ad04f1e086705c6316e56c6c899ecd264acda
pourya-momtaz <pouryamz19@gmail.com>
2023-07-04 20:47:25 +0330

    Add labels to print statements in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from circle import Circle
 from rectangle import Rectangle
 
 circle = Circle(5)
-print(circle.area())
+print("Circle area:", circle.area())
 
 rectangle = Rectangle(4, 6)
-print(rectangle.area())
+print("Rectangle area:", rectangle.area())
